def143fb9ae62bb71b98370cfbaa0fd326f88559
Maksym Kramar <maxim.kramar77@gmail.com>
2021-10-28 19:38:01 +0200

    Hw 5 finish

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,13 +1,17 @@
 const {Conflict} = require("http-errors");
-const {User} = require("../../models");
+const { User } = require("../../models");
+const gravatar = require('gravatar');
 
 const register = async(req, res)=>{
-    const {email, password} = req.body;
+    const {email, password, subscription} = req.body;
     const user = await User.findOne({email});
     if(user){
         throw new Conflict("Already register");
     }
-    const newUser = new User({ email });
+
+    const avatarURL = gravatar.url(email)
+
+    const newUser = new User({ email, subscription, avatarURL });
     
     newUser.setPassword(password);
     await newUser.save();
